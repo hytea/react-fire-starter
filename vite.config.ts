192c63a18e93b073fc772e9b089ab83eea8d56ec
Andrew Hyte <andrewdhyte@gmail.com>
2024-07-31 16:45:19 -0600

    Change how vite builds the site

--- vite.config.ts
+++ vite.config.ts
@@ -21,29 +21,4 @@ export default defineConfig({
     }),
     tsconfigPaths(),
   ],
-  build: {
-    lib: {
-      entry: path.resolve(__dirname, "src/index.tsx"),
-      name: "ReactFireStarter",
-      fileName: (format) => `react-fire-starter.${format}.js`,
-    },
-    rollupOptions: {
-      external: [
-        "react",
-        "react-dom",
-        "recoil",
-        "react-router-dom",
-        "firebase",
-      ],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          recoil: "Recoil",
-          "react-router-dom": "ReactRouterDOM",
-          firebase: "firebase",
-        },
-      },
-    },
-  },
 });
